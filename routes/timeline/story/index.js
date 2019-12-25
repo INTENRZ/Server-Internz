12d24cd0913b347858d2c09c5775e6fc9594f1b2
jungahshin <shinasong@naver.com>
2019-12-26 04:03:01 +0900

    package 추가, router 수정

--- routes/timeline/story/index.js
+++ routes/timeline/story/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/', require('./story'));
-router.use('/:storyIdx/scrap', require('./srap'));
+router.use('/:storyIdx/scrap', require('./scrap'));
 
 module.exports = router;
