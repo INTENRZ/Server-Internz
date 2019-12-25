4bd150678721c8b1d3f93c98b09c2d4273ae93af
jungahshin <shinasong@naver.com>
2019-12-26 00:54:39 +0900

    router timeline 추가

--- routes/timeline/story/story.js
+++ routes/timeline/story/story.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//router-> [GET]/timeline/{timelineIdx}/story
+router.get('/', async(req, res)=>{
+    try{
+
+    }catch(err){
+        console.log(err);
+    }
+});
+
 //router-> [POST]/timeline/{timelineIdx}/story
 router.post('/', async(req, res)=>{
     try{
