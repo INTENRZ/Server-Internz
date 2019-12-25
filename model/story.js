8aa7e8cf2f56835173e05da8a04e3ad37222e08a
ujusy <lsyeon1997@ajou.ac.kr>
2019-12-26 00:53:55 +0900

    add story model

--- model/story.js
+++ model/story.js
@@ -3,23 +3,27 @@ const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
 
-//story->create(스토리 댓글 생성),readAll(스토리 최신순 조회), read(스토리 조회순 조회),update(스토리 댓글 수정)
-//      ->delete(스토리 댓글 삭제)
+//story->create(스토리 댓글 생성),readAll(스토리 전체 중 최신순 조회), read(스토리 전체 중 조회순 조회),
+//       story_content_read(스토리 내용 조회),update(해당 스토리 댓글 수정)
+//       delete(해당 스토리 댓글 삭제)
 
 module.exports = {
-   create:() =>{
+    comment_create:() =>{
 
    },
-    readAll:() => {
+    storyAll_readAll:() => {
 
     },
-    read:() => {
+    storyAll_read:() => {
 
     },
-    update:()=>{
+    story_content_read:()=>{
 
     },
-    delete:()=>{
+    comment_update:()=>{
+
+    },
+    comment_delete:()=>{
 
     }
 };
