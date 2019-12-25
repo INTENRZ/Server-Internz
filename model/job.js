a90296215eac0ed1fb311df0158ac5f80f40a569
ujusy <lsyeon1997@ajou.ac.kr>
2019-12-25 20:21:17 +0900

    add job model

--- model/job.js
+++ model/job.js
@@ -2,3 +2,16 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
+
+//job->readAll(공고 전체 불러오기), read(지난 공고 불러오기)
+
+module.exports = {
+   
+    readAll:() => {
+
+    },
+    read:() => {
+
+    }
+    
+};
