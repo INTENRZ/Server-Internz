ecc714851923ec9ba2e9d00a4da3d0704c72c6a9
jungahshin <shinasong@naver.com>
2019-12-25 13:49:11 +0900

    model 추가

---
+++ model/home.js
@@ -0,0 +1,5 @@
+const util = require('../module/utils');
+const statusCode = require('../module/statusCode');
+const resMessage = require('../module/responseMessage');
+const db = require('../module/pool');
+
