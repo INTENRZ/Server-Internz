41b1145fa2e9f5c1e8331baec2189c600964b93c
jungahshin <shinasong@naver.com>
2019-12-26 01:03:20 +0900

    model user 수정

--- model/user.js
+++ model/user.js
@@ -2,3 +2,20 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
+
+//create(회원가입), check(로그인->닉네임, id 둘다 중복 확인), task_update(관심직군 수정), ability_update(보유역량 수정)
+
+module.exports = {
+    create:() =>{
+
+   },
+    task_update:() => {
+
+    },
+    ability_update:() => {
+
+    },
+    check:()=>{
+
+    }
+};
