7c5e94a5c9c398895ff7bf95899a9f08fa7351c6
jungahshin <shinasong@naver.com>
2019-12-26 00:49:03 +0900

    model profile 수정

--- model/profile.js
+++ model/profile.js
@@ -2,3 +2,23 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
+
+//profile->intro_create(한 줄 소개 작성), intro_delete(한 줄 소개 삭제), readAll(프로필 조회), task_update(관심 직군 수정), ability_update(보유 역량 수정)
+
+module.exports = {
+    intro_create:() => {
+
+    },
+    readAll:() => {
+
+    },
+    intro_delete:() => {
+
+    },  
+    task_update:() => {
+
+    },
+    ability_update:() => {
+
+    },
+};
