f35b330a42418162f8fa14054e9ac50e2d73f8d8
ujusy <lsyeon1997@ajou.ac.kr>
2019-12-26 00:40:23 +0900

    add story model

--- model/story.js
+++ model/story.js
@@ -3,3 +3,23 @@ const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
 
+//story->create(스토리 댓글 생성),readAll(스토리 최신순 조회), read(스토리 조회순 조회),update(스토리 댓글 수정)
+//      ->delete(스토리 댓글 삭제)
+
+module.exports = {
+   create:() =>{
+
+   },
+    readAll:() => {
+
+    },
+    read:() => {
+
+    },
+    update:()=>{
+
+    },
+    delete:()=>{
+
+    }
+};
