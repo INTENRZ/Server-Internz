315bf9cbd25c91b9cb186c6bbed300703376f999
jungahshin <shinasong@naver.com>
2019-12-26 05:08:43 +0900

    calender 추가

--- routes/calender/calender.js
+++ routes/calender/calender.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const util = require('../../module/utils');
+const statusCode = require('../../module/statusCode');
+const resMessage = require('../../module/responseMessage');
+const authUtil = require("../../module/authUtils");
+const calender = require("../../model/calender");
 
-//router-> [GET]/calender/{month}
-//여기서는 월만 받는
-router.get('/:month', async(req, res)=>{
+//router-> [GET]/calender/{day}
+//여기서는 월,일 받는
+router.get('/:day', async(req, res)=>{
     try{
 
     }catch(err){
