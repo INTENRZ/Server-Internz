f0e712f9e30227e86191d0b44a1cb1fb1112e09a
jiss02 <olo5112olo@naver.com>
2019-12-25 20:22:18 +0900

    add home model

--- model/home.js
+++ model/home.js
@@ -3,3 +3,10 @@ const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
 
+// home -> home(첫 홈 화면 띄우기)
+
+module.exports = {
+    home : () => {
+
+    }
+}
--- model/story.js
+++ model/story.js
@@ -2,3 +2,4 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
+
