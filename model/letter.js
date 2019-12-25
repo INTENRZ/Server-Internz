0c0e7bcfaa6e0e2ed26d6dd014041173e1fac97c
jiss02 <olo5112olo@naver.com>
2019-12-26 00:40:23 +0900

    add letter model

--- model/letter.js
+++ model/letter.js
@@ -2,3 +2,20 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
+
+//letter->readAll(쪽지를 나눈 사람들 조회), read(특정 사람과의 쪽지 목록 조회), delete(쪽지 삭제), create(쪽지 보내기)
+
+module.exports = {
+    readAll: () => {
+
+    },
+    read: () => {
+
+    },
+    delete: () => {
+
+    },
+    create: () => {
+
+    }
+}
