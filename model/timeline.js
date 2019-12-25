aa8d49925b1f475fbf4c8a77c5dde534f74d0d5a
jiss02 <olo5112olo@naver.com>
2019-12-26 00:55:29 +0900

    add timeline model

--- model/timeline.js
+++ model/timeline.js
@@ -2,3 +2,35 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
+
+// timeline
+// read(타임라인 조회), create(타임라인 생성), update(타임라인 수정), delete(타임라인 삭제)
+// story_read(타임라인 스토리 조회), story_create(타임라인 스토리 생성), story_update(타임라인 스토리 수정), story_delete(타임라인 스토리 삭제)
+
+module.exports = {
+    read: () => {
+
+    },
+    create: () => {
+
+    },
+    update: () => {
+
+    },
+    delete: () => {
+
+    },
+    story_read: () => {
+
+    },
+    story_create: () => {
+
+    },
+    story_update: () => {
+
+    },
+    story_delete: () => {
+
+    }
+
+}
