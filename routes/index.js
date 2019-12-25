4d329c335be145c0ca90329b62b510ba641a6109
jungahshin <shinasong@naver.com>
2019-12-26 03:59:54 +0900

    router 추가

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@ var router = express.Router();
 router.use('/user', require('./user'));
 router.use('/profile', require('./profile'));
 router.use('/letter', require('./letter'));
+router.use('/calender', require('./calender'));
+router.use('/home', require('./home'));
+router.use('/job', require('./job'));
+router.use('/story', require('./story'));
+router.use('/timeline', require('./timeline'));
 
 module.exports = router;
