a266b8720b135421e085cbaacfe87eb97c5a0119
ujusy <lsyeon1997@ajou.ac.kr>
2019-12-26 13:34:32 +0900

    add routes task

--- routes/user/task.js
+++ routes/user/task.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+const util = require('../../module/utils');
+const statusCode = require('../../module/statusCode');
+const resMessage = require('../../module/responseMessage');
+const User = require('../../model/user');
 //router-> [PUT]/user/task
 router.put('/', async(req, res)=>{
     try{
+		const {task_one, task_two, task_three} = req.body;
 
+        if(!task_one || !task_two || !task_three){
+            res.status(statusCode.BAD_REQUEST)
+            .send(util.successFalse(resMessage.NULL_VALUE));
+
+            return ;
+        }
+        User.task_update({task_one, task_two, task_three})
+        .then(({code, json})=> res.status(code).send(json))
+        .catch(err=>{
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR,
+                util.successFalse(resMessage.INTERNAL_SERVER_ERROR))
+        });
     }catch(err){
         console.log(err);
     }
