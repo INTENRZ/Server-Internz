4ff2ca88f16cb97dfc424dd2da70ef3ba76760dd
jungahshin <shinasong@naver.com>
2019-12-26 18:23:44 +0900

    scheduler model/router 추가

---
+++ routes/scheduler/scheduler.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+//router-> [PUT]/scheduler
+router.put('/', async(req, res)=>{
+    try{
+        
+    }catch(err){
+        console.log(err);
+    }
+});
+
+module.exports = router;
