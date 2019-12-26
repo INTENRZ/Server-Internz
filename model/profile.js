4dc18dd3afc69a127d3f1fdca8ba74acd3535f2a
jungahshin <shinasong@naver.com>
2019-12-27 01:22:36 +0900

    router/model 수정

--- model/profile.js
+++ model/profile.js
@@ -3,7 +3,7 @@ const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
 
-//profile->intro_create(한 줄 소개 작성), intro_delete(한 줄 소개 삭제), readAll(프로필 조회), task_update(관심 직군 수정), ability_update(보유 역량 수정)
+//profile->intro_create(한 줄 소개 작성), intro_delete(한 줄 소개 삭제), readAll(프로필 조회), task_update(관심 직군 수정)
 
 module.exports = {
     intro_create:() => {
@@ -17,8 +17,5 @@ module.exports = {
     },  
     task_update:() => {
 
-    },
-    ability_update:() => {
-
-    },
+    }
 };
--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/ability', require('./ability'));
 router.use('/intro', require('./intro'));
 router.use('/task', require('./task'));
 router.use('/', require('./profile'));
