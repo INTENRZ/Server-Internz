0005bc56a78c15ff5621ee585a4fde18d2f59fc1
ujusy <lsyeon1997@ajou.ac.kr>
2019-12-26 19:28:25 +0900

    change file name

--- routes/user/index.js
+++ routes/user/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.use('/signin', require('./signin'));
 router.use('/signup', require('./signup'));
-router.use('/ability', require('./ability'));
+router.use('/introduce', require('./introduce'));
 router.use('/task', require('./task'));
 
 module.exports = router;
