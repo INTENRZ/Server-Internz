5ef750d19f2a1da843f218b432fa823dbc537e99
jiss02 <olo5112olo@naver.com>
2019-12-27 01:58:25 +0900

    letter router 전체조회 구현

--- routes/letter/letter.js
+++ routes/letter/letter.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Letter = require('../../model/letter');
 
 //router-> [POST]/letter
 router.post('/', async(req, res)=>{
--- routes/letter/others.js
+++ routes/letter/others.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const Letter = require('../../model/letter');
+
 
 //router-> [GET]/letter/others
 router.get('/', async(req, res)=>{
     try{
-
+        console.log("시작")
+        const {userIdx} = req.body;
+        Letter.readAll(userIdx)
+        .then(({code, json})=>{
+            res.status(code).send(json);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
     }catch(err){
         console.log(err);
     }
