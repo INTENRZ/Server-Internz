81e6a5439427fc76de7e3bed24bf8dee1466d992
ujusy <lsyeon1997@ajou.ac.kr>
2019-12-27 02:12:59 +0900

    add profile readAll

--- model/profile.js
+++ model/profile.js
@@ -2,14 +2,37 @@ const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
 const db = require('../module/pool');
-
-//profile->intro_create(한 줄 소개 작성), intro_delete(한 줄 소개 삭제), readAll(프로필 조회), task_update(관심 직군 수정)
+const crypto = require('crypto');
+const encrypt = require('../module/encryption');
+const Auth = require('../module/jwt');
+const multer = require('multer');
+const upload = multer({dest:'uploads/'});
+//profile->intro_update(한 줄 소개 작성), intro_delete(한 줄 소개 삭제), readAll(프로필 조회), task_update(관심 직군 수정)
 
 module.exports = {
-    intro_create:() => {
+    intro_update:() => {
 
     },
-    readAll:() => {
+    readAll:({userIdx}) => {
+        return new Promise(async(resolve, reject)=>{
+            const checkQuery = `SELECT userIdx,nickname, task_one, task_two, task_three,front_image, back_image,introduce FROM user WHERE userIdx =?`;
+            const checkResult = await db.queryParam_Parse(checkQuery, [userIdx]);
+            const timeQuery = `SELECT * FROM timeline WHERE userIdx=?`;
+            const timeResult = await db.queryParam_Parse(timeQuery, [userIdx]);
+
+            const result = checkResult.concat(timeResult);
+            if(checkResult.length == 0){
+                resolve({
+                    code: statusCode.NOT_FOUND,
+                    json: util.successFalse(resMessage.NO_USER)
+                });
+                return ;
+            }
+            resolve({
+                code: statusCode.OK,
+                json: util.successTrue(resMessage.X_READ_ALL_SUCCESS("프로필"),result)
+            })
+        });
 
     },
     intro_delete:() => {
--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const util = require('../../module/utils');
+const statusCode = require('../../module/statusCode');
+const resMessage = require('../../module/responseMessage');
+const Profile = require('../../model/profile');
+const authUtils = require('../../module/authUtils');
+router.use('/',authUtils.isLoggedin);
 
 //router-> [GET]/profile
 router.get('/', async(req, res)=>{
     try{
-
+	 const userIdx = req.decoded.idx;
+        Profile.readAll({userIdx})
+        .then(({code,json})=> res.status(code).send(json))
+        .catch(err=>{
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR,
+                util.successFalse(resM
     }catch(err){
         console.log(err);
     }
