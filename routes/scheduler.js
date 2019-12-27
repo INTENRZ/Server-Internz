976dff75525c0d5d589a1a69a108bfd3b80a617f
jungahshin <shinasong@naver.com>
2019-12-27 15:16:36 +0900

    scheduler 수정

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -5,8 +5,6 @@ const db = require('../module/pool');
 const moment = require('moment');
 
 cron.schedule('0 0 0 * * *', async() => {//자정에
-    //d_day가 -이면 지나지 않은 --> d_day--
-    //d_day가 +이면 지난 것 --> d_day++
     const updateDdayQuery = 'UPDATE job SET d_day = d_day + 1';
     const insertTransaction = await db.Transaction(async(connection) => {
         const updateDdayResult = await connection.query(updateDdayQuery);
