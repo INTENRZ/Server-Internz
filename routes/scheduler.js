ca248b802626bf0d184ec05c47c3987b7b0e6837
jungahshin <shinasong@naver.com>
2019-12-27 15:15:22 +0900

    scheduler 수정

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -4,7 +4,6 @@ const cron = require('node-cron');
 const db = require('../module/pool');
 const moment = require('moment');
 
-//router-> [PUT]/scheduler
 cron.schedule('0 0 0 * * *', async() => {//자정에
     //d_day가 -이면 지나지 않은 --> d_day--
     //d_day가 +이면 지난 것 --> d_day++
