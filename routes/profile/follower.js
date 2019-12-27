7de709cf0c16a3198cf812d646f89a614b25370a
jiss02 <olo5112olo@naver.com>
2019-12-27 16:15:30 +0900

    follower list 조회 router 구현

--- routes/profile/follower.js
+++ routes/profile/follower.js
@@ -5,5 +5,23 @@ const statusCode = require('../../module/statusCode');
 const resMessage = require('../../module/responseMessage');
 const db = require('../../module/pool');
 const authUtils = require('../../module/authUtils');
+const Follow = require('../../model/follow');
+
+// router.use('/', authUtils.isLoggedin);
+
+//following -> [GET]/flollower
+router.get('/', (req, res)=> {
+    // const userIdx = req.decoded.idx;
+    const {userIdx} = req.body;
+    Follow.followerReadAll(userIdx)
+    .then(({code, json})=>{
+        res.status(code).send(json);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+    })
+});
 
 module.exports = router;
