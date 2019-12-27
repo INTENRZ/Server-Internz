7bc0d245bb06448e26301cc95f67c2ff8b8a1906
jiss02 <olo5112olo@naver.com>
2019-12-27 18:20:21 +0900

    following create, delete 추가

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/follower', require('./follower'));
+router.use('/following', require('./following'));
 router.use('/intro', require('./intro'));
 router.use('/task', require('./task'));
 router.use('/', require('./profile'));
-router.use('/follower', require('./follower'));
-router.use('/following', require('./following'));
+
 
 module.exports = router;
