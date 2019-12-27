4b8111c17f1c0ec2d5aa22b6640161aae4df6565
jiss02 <olo5112olo@naver.com>
2019-12-27 23:09:38 +0900

    scrap 기능 틀 구현

--- model/scrap.js
+++ model/scrap.js
@@ -1,7 +1,7 @@
 const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
-const db = require('../module/pool');
+const db = require('../module/poolAsync');
 
 //scrap-> create(스크랩), delete(스크랩취소)
 
@@ -10,6 +10,9 @@ module.exports = {
 
     },
     delete: () => {
+
+    },
+    readAll: () => {
 
     }
     
--- routes/timeline/story/scrap/scrap.js
+++ routes/timeline/story/scrap/scrap.js
@@ -1,11 +1,46 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
+const util = require('../../../../module/utils');
+const statusCode = require('../../../../module/statusCode');
+const resMessage = require('../../../../module/responseMessage');
+const Scrap = require('../../../../model/scrap');
+const au = require('../../../../module/authUtils');
+
+router.use('/', au.isLoggedin);
 
 //router-> [GET]/timeline/{timelineIdx}/story/{storyIdx}/scrap
 router.get('/', async(req, res)=>{
     try{
-
+        const usreIdx = req.decoded.idx;
+        const {storyIdx} = req.params;
+        Scrap.readAll({userIdx, storyIdx})
+        .then(({code, json})=>{
+            res.status(code).send(json);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
     }catch(err){
+        console.log(err);
+    }
+});
+
+router.post('/', (req, res)=> {
+    try{
+        const usreIdx = req.decoded.idx;
+        const {storyIdx} = req.params;
+        Scrap.create({userIdx, storyIdx})
+        .then(({code, json})=>{
+            res.status(code).send(json);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
+    } catch(err) {
         console.log(err);
     }
 });
@@ -13,7 +48,17 @@ router.get('/', async(req, res)=>{
 //router-> [DELETE]/timeline/{timelineIdx}/story/{storyIdx}/scrap/{scrapIdx}
 router.delete('/:scrapIdx', async(req, res)=>{
     try{
-
+        const usreIdx = req.decoded.idx;
+        const {storyIdx} = req.params;
+        Scrap.delete({userIdx, storyIdx})
+        .then(({code, json})=>{
+            res.status(code).send(json);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
     }catch(err){
         console.log(err);
     }
