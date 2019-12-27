d13b951b7c03eb2f081f77b18381f9b1fb262649
jiss02 <olo5112olo@naver.com>
2019-12-27 14:58:39 +0900

    letter mode 최근 쪽지 기능 추가 및 로직 수정

--- model/letter.js
+++ model/letter.js
@@ -20,18 +20,30 @@ Object.defineProperty(Array.prototype, 'flat', {
 
 module.exports = {
     readAll: (userIdx) => {
-        const innerq = `SELECT DISTINCT receiver, sender FROM ${TABLE} WHERE receiver = ${userIdx} OR sender = ${userIdx}`;
-        const sendData = db.queryParam_None(innerq)
+        // 유저와 주고 받은 상대방 찾기
+        const pickLetterQuery = `SELECT DISTINCT receiver, sender FROM ${TABLE} WHERE receiver = ${userIdx} OR sender = ${userIdx}`;
+        const sendData = db.queryParam_None(pickLetterQuery)
         .then( async (result) =>{
-            let check = result.map(it => Object.values(it));
-            check = check.flat(1);
-            check = Array.from(new Set(check));
-            check = check.filter(it=>it!==userIdx);
-            const letterq = `SELECT userIdx ,nickname FROM user WHERE userIdx IN (${check})`;
-            const letter_users = await dbsync.queryParam_None(letterq);
+            // 상대 유저 배열에 담기 (상대방 인덱스의 중복 제거)
+            let other = result.map(it => Object.values(it));
+            other = other.flat(1);
+            other = Array.from(new Set(other));
+            other = other.filter(it=>it!==userIdx);
+            // 쪽지를 나눈 사람들의 idx가 잠긴 배열 
+            const pickLetterUserQuery = `SELECT userIdx ,nickname FROM user WHERE userIdx IN (${other})`;
+            const userArray = await dbsync.queryParam_None(pickLetterUserQuery);
+            // 각 상대방 마다 최신 쪽지 파악
+            const recentMsgArray = [];
+            for(var i in other){
+                const recMsgQ = `SELECT content FROM ${TABLE} WHERE receiver = ${other[i]} OR sender = ${other[i]} ORDER BY created_date LIMIT 1`
+                const recMsgResult = await dbsync.queryParam_None(recMsgQ);
+                recentMsgArray.push(recMsgResult[0]);
+            }
+            // userArray와 recentMsgArray 오브젝트 끼리 합치기
+            const letterUserList = userArray.map((it, idx) => Object.assign(it, recentMsgArray[idx]));
             return {
                 code: statusCode.OK,
-                json: util.successTrue(resMessage.X_READ_ALL_SUCCESS(NAME), letter_users)
+                json: util.successTrue(resMessage.X_READ_ALL_SUCCESS(NAME), letterUserList)
             }
         })
         .catch(err=>{
@@ -40,8 +52,8 @@ module.exports = {
         return sendData;
     },
     read: ({receiver, sender}) => {
-        const q = `SELECT * FROM ${TABLE} WHERE sender IN (?,?) ORDER BY created_date ASC`;
-        const v = [receiver, sender];
+        const q = `SELECT * FROM ${TABLE} WHERE sender IN (?,?) AND receiver IN (?,?) ORDER BY created_date ASC`;
+        const v = [receiver, sender, receiver, sender];
         const sendData = db.queryParam_Parse(q, v)
         .then(result => {
             if(result.length === 0){
