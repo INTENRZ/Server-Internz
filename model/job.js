d479e41ce13bdacf57c25aeac82de948e8d2e497
jiss02 <olo5112olo@naver.com>
2019-12-27 21:07:53 +0900

    job filter 적용

--- model/job.js
+++ model/job.js
@@ -1,7 +1,8 @@
 const util = require('../module/utils');
 const statusCode = require('../module/statusCode');
 const resMessage = require('../module/responseMessage');
-const db = require('../module/pool');
+const dbsync = require('../module/pool');
+const db = require('../module/poolAsync');
 
 //job->readAll(공고 전체 불러오기), read(지난 공고 불러오기), filter(직무 1개 선택하면 나의 관심직무와 일치하면 필터)
 
@@ -23,8 +24,25 @@ module.exports = {
     read: () => {
 
     },
-    filter: () => {
-
+    filter: ({task}) => {
+        const pickTaskQuery = `SELECT * FROM job WHERE task1 = task OR task2 = task OR task3 = task`;
+        const sendData = db.queryParam_None(pickTaskQuery)
+        .then( async (result) => {
+            if(result.legnth === 0){
+                return {
+                    code: statusCode.OK,
+                    json: util.successFalse("필터와 일치하는 공고가 없습니다.")
+                }
+            }
+            return {
+                code: statusCode.OK,
+                json: util.successTrue(`${task} 필터 조회 성공`, result)
+            }
+        })
+        .catch(err => {
+            throw err;
+        });
+        return sendData
     }
 
 };
