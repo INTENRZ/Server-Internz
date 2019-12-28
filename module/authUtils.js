6a6e276f5f02e71fbcd8841518fc2c8aae7f07d2
jungahshin <shinasong@naver.com>
2019-12-28 23:00:47 +0900

    authUtils 수정

--- module/authUtils.js
+++ module/authUtils.js
@@ -21,10 +21,10 @@ const authUtil = {
 
             if (user == -3) {
                 //유효기간이 지난 토큰일 때
-                return res.json(util.successFalse(resMessage.EXPRIED_TOKEN));
+                return res.json(util.successFalse(statusCode.DONE_TOKEN,resMessage.EXPRIED_TOKEN));
             } else if (user == -2) {
                 //잘못 형식의 토큰(키 값이 다르거나 등등)일 때
-                return res.json(util.successFalse(resMessage.INVALID_TOKEN));
+                return res.json(util.successFalse(statusCode.NOT_TOKEN_FORMAT, resMessage.INVALID_TOKEN));
             } else {
                 //req.decoded에 확인한 토큰 값 넣어줌
                 req.decoded = user;
