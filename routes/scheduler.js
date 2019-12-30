b7beb86585c570c6e3405601a27e298c7c5c3871
jungahshin <shinasong@naver.com>
2019-12-30 16:40:37 +0900

    scheduler ispast 추가

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -6,8 +6,10 @@ const moment = require('moment');
 
 cron.schedule('0 0 0 * * *', async() => {//자정에
     const updateDdayQuery = 'UPDATE job SET d_day = d_day + 1';
+    const updateIsPastQuery = 'UPDATE job SET ispast = 1 WHERE d_day > 0';
     const insertTransaction = await db.Transaction(async(connection) => {
         const updateDdayResult = await connection.query(updateDdayQuery);
+        const updateIsPastResult = await connection.query(updateIsPastQuery);
     });
 })
 
