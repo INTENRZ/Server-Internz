87dc3497079ed9aa57c2b66c6e9f050280283d2c
jungahshin <shinasong@naver.com>
2019-12-31 13:25:32 +0900

    story model 수정

--- model/story.js
+++ model/story.js
@@ -5,8 +5,8 @@ const db = require('../module/pool');
 const moment = require('moment');
 
 //story->create(스토리 댓글 생성),readAll(스토리 전체 중 최신순 조회), read(스토리 전체 중 조회순 조회),
-//       story_content_read(스토리 내용 조회),update(해당 스토리 댓글 수정)
-//       delete(해당 스토리 댓글 삭제)
+//       story_content_read(스토리 내용 조회), comment_update(해당 스토리 댓글 수정)
+//       comment_delete(해당 스토리 댓글 삭제)
 
 const story = "스토리";
 
