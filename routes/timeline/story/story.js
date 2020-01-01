d62d1971fa90d0883099ebbdb937c952a36360d6
jiss02 <olo5112olo@naver.com>
2020-01-02 01:58:01 +0900

    timeline story router 복구

--- routes/timeline/story/story.js
+++ routes/timeline/story/story.js
@@ -1,62 +1,95 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
 const util = require('../../../module/utils');
 const statusCode = require('../../../module/statusCode');
 const resMessage = require('../../../module/responseMessage');
-const authUtil = require("../../../module/authUtils");
-const story = require('../../../model/story');
+const Timeline = require('../../../model/timeline');
+const au = require('../../../module/authUtils');
 
-//router-> [GET]/story/{storyIdx}
-router.get('/:storyIdx', authUtil.isLoggedin, async(req, res)=>{
+router.use('/', au.isLoggedin);
+
+//router-> [GET]/timeline/{timelineIdx}/story
+router.get('/', async(req, res)=>{
     try{
-        const storyIdx = req.params.storyIdx;
-        const user = req.decoded.idx;
-        story.story_content_read(user, storyIdx)
-        .then(({code, json}) => {
+        const userIdx = req.decoded.idx;
+        // const userIdx = req.body.userIdx;
+        const {timelineIdx} = req.params;
+        Timeline.story_read({userIdx, timelineIdx})
+        .then(({code, json})=>{
             res.status(code).send(json);
         })
         .catch(err => {
             console.log(err);
-            res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
-        })
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
     }catch(err){
         console.log(err);
     }
 });
 
-//router-> [POST]/story/category
-router.post('/category', async(req, res)=>{
+//router-> [POST]/timeline/{timelineIdx}/story
+router.post('/', async(req, res)=>{
     try{
-        const Category = req.body.category;
-        if(!Category){
-            res.status(statusCode.OK).send(util.successFalse(statusCode.MORE_VALUE_NEED, resMessage.NULL_VALUE));
-            return;
+        const userIdx = req.decoded.idx;
+        const {timelineIdx} = req.params;
+        const {title, content} = req.body;
+        if(!title||!content){
+            res.status(statusCode.OK)
+            .send(util.successFalse(resMessage.NULL_VALUE));
         }
-        story.story_category(Category)
-        .then(({code, json}) => {
+        Timeline.story_create({userIdx, timelineIdx,title, content})
+        .then(({code, json})=>{
             res.status(code).send(json);
         })
-        .catch(err => {
+        .catch(err =>{
             console.log(err);
-            res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
-        })
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
     }catch(err){
         console.log(err);
     }
 });
 
-//router-> [POST]/story/category/sort
-router.post('/category/sort', async(req, res)=>{
+//router-> [PUT]/timeline/{timelineIdx}/story/{storyIdx}
+router.put('/:storyIdx', async(req, res)=>{
     try{
-        const category = req.body.category;
-        const sort = req.body.sort;
-        story.story_category_sort(sort, category)
-        .then(({code, json}) => {
+        const userIdx = req.decoded.idx;
+        const {storyIdx} = req.params;
+        const {title, content} = req.body;
+        if(!title||!content){
+            res.status(statusCode.OK)
+            .send(util.successFalse(resMessage.NULL_VALUE));
+        }
+        Timeline.story_update({userIdx, storyIdx, title, content})
+        .then(({code, json})=>{
             res.status(code).send(json);
         })
-        .catch(err => {
+        .catch(err =>{
             console.log(err);
-            res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.successFalse(statusCode.INTERNAL_SERVER_ERROR, resMessage.INTERNAL_SERVER_ERROR));
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+        });
+    }catch(err){
+        console.log(err);
+    }
+});
+
+//router-> [DELETE]/timeline/{timelineIdx}/story/{storyIdx}
+router.delete('/:storyIdx', async(req, res)=>{
+    try{
+        const userIdx = req.decoded.idx;
+        const {storyIdx} = req.params;
+        // const {userIdx} = req.body;
+        Timeline.story_delete({userIdx, storyIdx})
+        .then(({code, json})=>{
+            res.status(code).send(json);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.successFalse(resMessage.INTERNAL_SERVER_ERROR));
         })
     }catch(err){
         console.log(err);
